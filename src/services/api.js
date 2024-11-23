fb7d3fed8ee9404ce26c9952709f2889d2e90598
Alwaylittl <jaariasr@alumnos.unex.es>
2024-11-23 20:33:04 +0100

    SCRUM-170-CU8-IMP.-Detalle-contenido

--- src/services/api.js
+++ src/services/api.js
@@ -1,1 +1,15 @@
 
+// Obtener detalles de un contenido (GET method)
+export const obtenerContenidoPorId = async (id) => {
+    try {
+        const response = await fetch(${API_URLS.contenidos}/${id}, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        return await handleResponse(response);
+    } catch (error) {
+        console.error("Error al obtener el contenido:", error);
+    }
+};
